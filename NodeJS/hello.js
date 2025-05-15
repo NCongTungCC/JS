e6c80e148a8888971bda8de87fda7a33df612848
congtung <tung9x9z123@gmail.com>
2025-05-15 14:03:14 +0700

    update

--- NodeJS/hello.js
+++ NodeJS/hello.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
 
 inquirer.prompt([
   { type: 'input', name: 'username', message: 'Tên bạn là gì?' }
@@ -6,3 +8,26 @@ inquirer.prompt([
   console.log('Xin chào', answers.username);
 });
 process.stdout.write('Xin chào');
+
+const data = fs.readFile(path.join(__dirname,'hello.txt'), 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+const pathHello = path.join(__dirname, 'hello.txt');
+
+console.log(pathHello);
+console.log(process.cwd());
+
+const write = fs.writeFile(pathHello, 'Hello', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Ghi file thành công');
+});
+
+console.log(__filename);
