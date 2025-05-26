00cd582bbbc621c9bca2fc78acb6564cb5f5e29c
congtung <tung9x9z123@gmail.com>
2025-05-26 09:10:11 +0700

    vi du

--- NodeJS/baitap.js
+++ NodeJS/baitap.js
@@ -5,10 +5,29 @@
 //     E() // bat dong bo
 //     F() // dong bo
 // }
+async function demo() {
+    console.log("A");
+    setTimeout(() => {
+        console.log("B");
+    }, 1000);
+    await new Promise(resolve => {
+    setTimeout(() => {
+        console.log("C");
+        resolve();
+    }, 0);
+});
+     setTimeout(() => {
+        console.log("E");
+    }, 1000);
+    console.log("F");
+}
+demo();
 
 try {
     let i = 0;
     i = i + x;
 } catch(err) {
     console.log("looix");
+} finally {
+    console.log("finally");
 }
